107106adf3ae57e515cca8886e3a5c28d0fe2af4
Marcus Balbi <balbimarcus@gmail.com>
2022-05-31 22:16:35 -0300

    client setup finished

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,20 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Fib from "./Fib";
+import OtherPage from "./OtherPage";
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Router>
+      <h1>FIB CALCULATOR</h1>
+      <nav>
+        <Link to="/">Home | </Link>
+        <Link to="/otherpage">Otherpage</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Fib />} />
+        <Route path="/otherpage" element={<OtherPage />} />
+      </Routes>
+    </Router>
   );
 }
 
