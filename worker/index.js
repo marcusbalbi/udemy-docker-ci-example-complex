d4e23edaef16dbc6f1ed1941364e4bde5518e974
Marcus Balbi <balbimarcus@gmail.com>
2022-05-31 21:16:00 -0300

    worker setup

---
+++ worker/index.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+const keys = require('./keys')
+const { createClient } = require('redis');
+
+const client = createClient({ url: `redis://${keys.redisHost}:${keys.redisPort}` });
+client.connect().then(() => {});
+
+const sub = client.duplicate();
+
+function fib(index) {
+  if (index < 2) { return 1; }
+
+  return fib(index -1) + fib(index - 2);
+}
+
+sub.on('message', (channel, message) => {
+  client.hSet('values', message, fib(parseInt(message)));
+});
+
+sub.subscribe('insert');
